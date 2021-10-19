556f0dc7a1d6c6095f7f5b6b59d871fd591d8bc7
Bruce <brdrane3121@gmail.com>
2021-10-20 10:24:11 +1100

    deploying our first model hey why not again!

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from types import resolve_bases
 from flask import Flask, request, url_for, redirect, render_template
 import pandas as pd
 import pickle
-from flask_cors import CORS
+from Flask-Cors import CORS
 app = Flask(__name__)
 CORS(app)
 
